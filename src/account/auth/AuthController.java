ed82c8ef159cf941b02e3db25bdf7985b8cece87
Damian Ciepiela <dciepiela@yahoo.com>
2022-01-15 22:41:04 +0100

    feat: basic spring authentication

--- src/account/auth/AuthController.java
+++ src/account/auth/AuthController.java
@@ -3,6 +3,7 @@ package account.auth;
 
 import account.auth.dto.UserAuthGetDto;
 import account.auth.dto.UserAuthPostDto;
+import account.auth.exceptions.UserExistException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,18 +16,61 @@ import javax.validation.Valid;
 
 @Validated
 @RestController
-@RequestMapping("/api/auth/signup")
+@RequestMapping("/api/auth")
 public class AuthController {
 
-    private final AuthService authService;
+    private final UserService userService;
 
-    public AuthController(AuthService authService) {
-        this.authService = authService;
+    public AuthController(UserService userService) {
+        this.userService = userService;
     }
 
-    @PostMapping()
-    public ResponseEntity<UserAuthGetDto> signUser(@RequestBody @Valid UserAuthPostDto userAuthPostDto) {
-        return new ResponseEntity<>(authService.signUser(userAuthPostDto), HttpStatus.OK);
+    @PostMapping("/signup")
+    public ResponseEntity<JBResponse> signUpUser(@RequestBody @Valid UserAuthPostDto userAuthPostDto) {
+        return new ResponseEntity<>(new JBResponse(userService.signUpUser(userAuthPostDto)), HttpStatus.OK);
+    }
+
+    private final static class JBResponse {
+        private static long id;
+
+        private String name;
+        private String lastname;
+        private String email;
+
+        public JBResponse(UserAuthGetDto authGetDto) {
+            JBResponse.id++;
+            this.name = authGetDto.getName();
+            this.lastname = authGetDto.getLastname();
+            this.email = authGetDto.getEmail();
+        }
+
+        public long getId() {
+            return JBResponse.id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getLastname() {
+            return lastname;
+        }
+
+        public void setLastname(String lastname) {
+            this.lastname = lastname;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
     }
 
 }
