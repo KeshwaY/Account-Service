e6eff0b2c428ac62deebf0e81a41f77b4db86c11
Damian Ciepiela <dciepiela@yahoo.com>
2022-01-16 11:10:13 +0100

    chore: disable mapper for JBa tests

--- src/account/auth/dto/UserAuthMapper.java
+++ src/account/auth/dto/UserAuthMapper.java
@@ -1,14 +1,11 @@
 package account.auth.dto;
 
-import account.auth.User;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
-@Mapper(componentModel = "spring")
 public interface UserAuthMapper {
-
+/*
     @Mapping(target = "id", ignore = true)
     User userAuthPostDtoToUser(UserAuthPostDto userAuthPostDto);
     UserAuthGetDto userToUserAuthGetDto(User userAuth);
+
+ */
 
 }
