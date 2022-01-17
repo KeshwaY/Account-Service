a22af1d20e88ffe71f138b4227e32bf7d1e6aaf2
Damian Ciepiela <dciepiela@yahoo.com>
2022-01-17 19:48:12 +0100

    fix: 404 in user when not found

--- src/account/auth/user/exceptions/UserDoesNotExistsException.java
+++ src/account/auth/user/exceptions/UserDoesNotExistsException.java
@@ -3,6 +3,6 @@ package account.auth.user.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "User does not exists!")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User not found!")
 public class UserDoesNotExistsException extends Exception {
 }
