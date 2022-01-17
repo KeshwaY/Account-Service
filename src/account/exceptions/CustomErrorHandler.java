8a04e6f0cf90f16b4dcf485ac42e9483378c0b4c
Damian Ciepiela <dciepiela@yahoo.com>
2022-01-17 10:44:58 +0100

    feat: handling constraint violations

--- src/account/exceptions/CustomErrorHandler.java
+++ src/account/exceptions/CustomErrorHandler.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
@@ -18,7 +20,7 @@ public class CustomErrorHandler{
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<RequestValidationExceptionDTO> handleConstraintViolationException(
+    public ResponseEntity<RequestValidationExceptionDTO> handleMethodArgumentNotValidException(
             MethodArgumentNotValidException ex,
             HttpServletRequest httpServletRequest
     ) {
@@ -32,5 +34,24 @@ public class CustomErrorHandler{
         );
         return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
     }
-
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<RequestValidationExceptionDTO> handleConstraintViolationException(
+            ConstraintViolationException ex,
+            HttpServletRequest httpServletRequest
+    ) {
+        StringBuilder builder = new StringBuilder();
+        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+            builder.append(violation.getMessage()).append(", ");
+        }
+        builder.replace(builder.length() - 2, builder.length(), "");
+        RequestValidationExceptionDTO dto = new RequestValidationExceptionDTO(
+                Timestamp.valueOf(LocalDateTime.now()),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                builder.toString(),
+                httpServletRequest.getRequestURI()
+        );
+        return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+    }
 }
